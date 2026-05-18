2ad763fb35a0469ded022eb79627d41e7eb0c54e
adesa <adesanyaenoch@gmail.com>
2026-05-18 19:54:15 +0100

    Initial Commit

---
+++ Minimal/MinimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalApp.swift
+//  Minimal
+//
+//  Created by Enoch Adesanya on 18/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MinimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MinimalTests/MinimalTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalTests.swift
+//  MinimalTests
+//
+//  Created by Enoch Adesanya on 18/05/2026.
+//
+
+import Testing
+@testable import Minimal
+
+struct MinimalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
